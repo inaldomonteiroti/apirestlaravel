b1840d96de833f16b4f3e0c73e09000c1a66cec9
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2021-09-25 11:26:09 -0300

    Projeto

--- app/Veiculo.php
+++ app/Veiculo.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Veiculo extends Model
 {
     protected $fillable = [
-        "marca","modelo", "ano", "preco"    ];
+    	"marca", "modelo", "ano", "preco"
+    ];
 }
